260be3022beaed16c25d9ea7695badf0f8b90a24
unknown <tech@my-caisse.fr>
2024-03-13 18:04:30 +0100

    add new repository

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,51 @@
+const {test, expect} = require('@playwright/test');
+// const {expect} = require('../playwright.config');
+
+test('Page Playwright test', async ({page})=> 
+{
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+
+
+});
+
+
+
+test.only('Browser Context Playwright test', async ({browser})=> 
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    const cardTitles = page.locator(".card-body a");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    // await page.locator('#username').type("rahulshettyacademy");
+    // await page.locator('#username').type("rahulshetty");
+    await userName.type("rahulshetty");
+    await page.locator("[type='password']").type("learning");
+    // await page.locator("#terms").click();
+    // await page.locator('#signInBtn').click();
+    await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await cardTitles.first().textContent());
+    console.log(await cardTitles.nth(1).textContent());
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+
+
+});
+
+
+
+
+
+
+
